4e136c90b44c1327f324c4290b662a642194a30c
Sudhanshu Jha <150656081+Sudhanshu012588@users.noreply.github.com>
2024-07-05 17:09:13 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
             <div className='left-0'>
 
                 <div className=" px-2 h-10  w-32 py-2 bg-blue-200 border rounded-full text-black flex justify-center items-center font-bold" ><a className="flex justify-center items-center font-bold" href='https://github.com/Sudhanshu012588' target='_blank'>
-                  <img className='h-8 mx-3 ' src="../public/social.png" alt="GitHub" />
+                  <img className='h-8 mx-3 ' src="./public/social.png" alt="GitHub" />
                         GitHub
                 </a>
                 </div>
